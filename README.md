f993125cc5a40e0e4495bf74d6a2b7efff113683
Michael Rabbitt <mrabbitt@oneis3.com>
2016-10-14 13:24:58 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Background  Info
+* [Festival Site FAQ w/ Map](http://treasureislandfestival.com/2016/festival-faq/)
+* Schedule
+    * [Saturday, October 15](http://lineup.treasureislandfestival.com/events/2016/10/15/)
+    * [Sunday, October 16](http://lineup.treasureislandfestival.com/events/2016/10/16/)
+* [Google Maps Pin @ 37.821266, -122.363837 ](https://www.google.com/maps/place/37%C2%B049'16.6%22N+122%C2%B021'49.8%22W/@37.8212702,-122.3660257,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d37.821266!4d-122.363837?hl=en)
+* Dark Sky forecast data:  <https://api.darksky.net/forecast/SECRET_KEY/37.821266,-122.363837>
+
---
+++ timf_schedule.bookmarklet.js
@@ -0,0 +1,39 @@
+(function (window) {
+    var events = [];
+    var timeRegex = /(\d{1,2}:\d{2})\s+-\s+(\d{1,2}:\d{2})/;
+    $('.ds-event-container').each(function(i, e) {
+        var performer = $(this).find('.ds-event-title').text().trim();
+
+        // Construct start and end time.
+        var dayText = $('.ds-arrange > div > span').text().trim();
+        var timeText = $(this).find('.ds-time-range').text().trim();
+        var match = timeRegex.exec(timeText);
+        var startTime = Date.parse(dayText + ' 2016 ' + match[1] + ' PM');
+        var endTime = Date.parse(dayText + ' 2016 ' + match[2] + ' PM');
+
+        // Parse Stage name.
+        var stageDiv = $(this).parents('.ds-stage');
+        var stageName = '????';
+        if ($(stageDiv).hasClass('ds-stage1')) {
+            stageId = "1";
+        } else if ($(stageDiv).hasClass('ds-stage2')) {
+            stageId = "2";
+        } else if ($(stageDiv).hasClass('ds-stage3')) {
+            stageId = "3";
+        }
+
+        events.push({
+            startTime: startTime,
+            endTime: endTime,
+            performer: performer,
+            stageId: stageId,
+        });
+    });
+
+    // FIXME Sort by time?
+
+    var eventsJson = JSON.stringify(events);
+    alert(eventsJson);
+    console.log(eventsJson);
+})(window);
+
