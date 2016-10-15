16be619681b7204e42201335c03b98a76553f066
Michael Rabbitt <mrabbitt@oneis3.com>
2016-10-15 01:01:39 -0700

    Added Node.js daemon which will fetch forecast data from Dark Sky and push to github pages every 15 minutes.

---
+++ forecast-daemon.js
@@ -0,0 +1,70 @@
+var https = require('https'),
+    path = require('path'),
+    fs = require('fs'),
+    ghpages = require('gh-pages'),
+    CronJob = require('cron').CronJob,
+    log4js = require('log4js');
+
+var logger = log4js.getLogger();
+
+if (process.env.DARK_SKY_SECRET_KEY === undefined) {
+    throw new Error('Environment variable missing: DARK_SKY_SECRET_KEY');
+}
+
+var fetchForecast = function(destFilePath, successCallback) {
+    var requestOptions = {
+        method: 'GET',
+        hostname: 'api.darksky.net',
+        port: 443,
+        path: '/forecast/' + process.env.DARK_SKY_SECRET_KEY +  '/37.821266,-122.363837',
+    };
+    var req = https.request(requestOptions, function (response) {
+        logger.info('entered handler for forecast data');
+
+        if (response.statusCode != 200) {
+            logger.error('Invalid response code=' + response.statusCode + ' response=' + response);
+            return;
+        }
+
+        // Verify request headers echo'd back in response body.
+        var respData = '';
+        response.on('data', function(chunk) {
+            respData += chunk;
+        });
+        response.on('end', function() {
+            logger.info('got response body');
+            fs.writeFileSync(destFilePath, respData);
+            logger.info('Contents written to: %s', destFilePath);
+            successCallback();
+        });
+    });
+
+    req.on('error', function(e) {
+        logger.error('problem with request: %s', e.message);
+    });
+
+    req.end();
+};
+
+var publishPages = function(dirPath) {
+    ghpages.publish(dirPath, {
+        logger: function(message) {
+            logger.info(message);
+         }
+    }, function(error) {
+        logger.info(error);
+    });
+};
+
+logger.info('Scheduling forecast fetch job to run every 15 minutes.');
+var job = new CronJob('00 0,15,30,45 * * * *', function() {
+    logger.info('*** forecast cronjob started.');
+    fetchForecast(path.join(__dirname, 'public/data/forecast.json'), function() {
+        publishPages(path.join(__dirname, 'public'));
+    });
+}, function() {
+    logger.info('*** forecast cronjob stopped.');
+},
+    true, /* Start the job right now */
+    'America/Los_Angeles' /* Time zone of this job. */
+);
